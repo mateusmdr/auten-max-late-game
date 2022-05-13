fd5070a19d50fb5b6524df80fb25e78dd14d4414
Mateus Rezende <mateusmendoncadr@gmail.com>
2022-05-13 04:02:58 -0300

    fix insights comparison

--- app/Http/Controllers/InsightsController.php
+++ app/Http/Controllers/InsightsController.php
@@ -26,46 +26,46 @@ class InsightsController extends Controller
         $endOfMonth = today()->endOfMonth()->toDateString();
         
         $totalUsers = 
-            User::where('is_blocked','=','false')
-                ->where('is_admin','=','false')
+            User::where('is_blocked',false)
+                ->where('is_admin',false)
                 ->count();
         $todayUsers = 
-            User::where('is_blocked','=','false')
-                ->where('is_admin','=','false')
+            User::where('is_blocked',false)
+                ->where('is_admin',false)
                 ->whereDate('created_at', '>=', $today)
                 ->whereDate('created_at','<',$tomorrow)
                 ->count();
         $monthUsers = 
-            User::where('is_blocked','=','false')
-                ->where('is_admin','=','false')
+            User::where('is_blocked',false)
+                ->where('is_admin',false)
                 ->whereDate('created_at', '>=', $startOfMonth)
                 ->whereDate('created_at', '<', $endOfMonth)
                 ->count();
 
         $totalPayments = 
-            Payment::where('is_pending','=','false')
+            Payment::where('is_pending',false)
                 ->sum('price');
         $todayPayments = 
-            Payment::where('is_pending','=','false')
+            Payment::where('is_pending',false)
                 ->whereDate('datetime', '>=', $today)
                 ->whereDate('datetime', '<', $tomorrow)
                 ->sum('price');
         $monthPayments = 
-            Payment::where('is_pending','=','false')
+            Payment::where('is_pending',false)
                 ->whereDate('datetime', '>=', $startOfMonth)
                 ->whereDate('datetime', '<', $endOfMonth)
                 ->sum('price');
 
         $totalTournaments = 
-            Tournament::where('is_approved','=','true')
+            Tournament::where('is_approved',true)
                 ->count();
         $todayTournaments = 
-            Tournament::where('is_approved','=','true')
+            Tournament::where('is_approved',true)
                 ->whereDate('date', '>=', $today)
                 ->whereDate('date', '<', $tomorrow)
                 ->count();
         $monthTournaments = 
-            Tournament::where('is_approved','=','true')
+            Tournament::where('is_approved',true)
                 ->whereDate('date', '>=', $startOfMonth)
                 ->whereDate('date', '<=', $endOfMonth)
                 ->count();
