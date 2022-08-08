cba0852cca67d8d52e0c54c653af87c09a081f99
Mateus Rezende <mateusmendoncadr@gmail.com>
2022-08-08 16:38:16 -0300

    modal de ativação de notificação para torneios recorrentes

--- resources/js/views/components/client/Tournaments/EnableNotificationModal.vue
+++ resources/js/views/components/client/Tournaments/EnableNotificationModal.vue
@@ -2,39 +2,94 @@
 	<Modal
         modalTitle= "Ativar"
         modalIcon="alarm_add"
-        :titleColor="tournament.is_recurrent ? '#F5A847' : '#05F28E'"
+        :titleColor="tournament.isRecurrent ? '#F5A847' : '#05F28E'"
         submitModalText= "Concluir"
-        :width="tournament.is_recurrent ? 55 : 30"
+        :width="tournament.isRecurrent ? 55 : 30"
         @submit="submit"
         ref="modal"
         noButton
         @close="$emit('close')"
 	>
         <div class="row mb-3">
-            <div class="col-12 mb-3">
-                <h4>Quando você deseja ser notificado?</h4>
+            <div :class="tournament.isRecurrent ? 'col-6 row align-content-start' : 'col-12 row'">
+                <div class="col-12 mb-3">
+                    <h4>Quando você deseja ser notificado?</h4>
+                </div>
+                <div class="col-12 mb-3">
+                    <Checkbox
+                        label="No início das inscrições"
+                        v-model="inputs.before"
+                    />
+                </div>
+                <div class="col-12">
+                    <Checkbox
+                        label="Antes do final das inscrições"
+                        v-model="inputs.after"
+                    />
+                </div>
+                <div class="col-6 mt-4" v-if="inputs.after">
+                    <TimeInput
+                        v-model="inputs.interval"
+                        :default="this.end"
+                    />
+                </div>
             </div>
-            <div class="col-12 mb-3">
-                <Checkbox
-                    label="No início das inscrições"
-                    v-model="inputs.before"
+            <div class="col-6 align-content-start" v-if="tournament.isRecurrent">
+                <div class="mb-3">
+                    <h4>Para quais ocorrências?</h4>
+                </div>
+                <RadioGroup
+                    :options="options"
+                    v-model="inputs.option"
                 />
+                <div v-if="inputs.option === 'custom'" class="row weekdays-container my-3 gy-3">
+                    <div class="col-12">
+                        <h4>Em quais dias da semana?</h4>
+                    </div>
+                    <div class="col-4">
+                        <Checkbox
+                            v-model="inputs.weekDays[0]"
+                            label="Domingo"
+                        />
+                    </div>
+                    <div class="col-4">
+                        <Checkbox
+                            v-model="inputs.weekDays[1]"
+                            label="Segunda"
+                        />
+                    </div>
+                    <div class="col-4">
+                        <Checkbox
+                            v-model="inputs.weekDays[2]"
+                            label="Terça"
+                        />
+                    </div>
+                    <div class="col-4">
+                        <Checkbox
+                            v-model="inputs.weekDays[3]"
+                            label="Quarta"
+                        />
+                    </div>
+                    <div class="col-4">
+                        <Checkbox
+                            v-model="inputs.weekDays[4]"
+                            label="Quinta"
+                        />
+                    </div>
+                    <div class="col-4">
+                        <Checkbox
+                            v-model="inputs.weekDays[5]"
+                            label="Sexta"
+                        />
+                    </div>
+                    <div class="col-4">
+                        <Checkbox
+                            v-model="inputs.weekDays[6]"
+                            label="Sábado"
+                        />
+                    </div>
+                </div>
             </div>
-            <div class="col-12">
-                <Checkbox
-                    label="Antes do final das inscrições"
-                    v-model="inputs.after"
-                />
-            </div>
-            <div class="col-6 mt-4" v-if="inputs.after">
-                <TimeInput
-                    v-model="inputs.interval"
-                    :default="this.end"
-                />
-            </div>
-        </div>
-        <div v-if="tournament.is_recurrent">
-        
         </div>
 	</Modal>
 </template>
@@ -55,6 +110,13 @@ export default {
     props: {
         tournament: Object
     },
+    created() {
+        this.options = [
+            {text: 'Todas as ocorrências', value: 'all'},
+            {text: 'Apenas esta ocorrência', value: 'one'},
+            {text: 'Personalizado', value: 'custom'},
+        ]
+    },
     mounted() {
 		this.$refs.modal.openModal();
 
@@ -67,22 +129,35 @@ export default {
             inputs: {
                 before: false,
                 after: false,
-                interval: null
+                interval: null,
+                weekDays: Array(6).fill(false),
+                option: 'one'
             },
             errors: {
-                before: false,
-                after: false,
-                interval: null
+                before: null,
+                after: null,
+                interval: null,
+                weekDays: null
             }
         }
     },
     methods: {
         submit() {
+            const schedule = this.tournament.isRecurrent ?
+                "* * " +
+                this.inputs.weekDays
+                    .map((item, index) => index)
+                    .filter((index) => this.inputs.weekDays[index])
+                    .join(',')
+                : undefined;
+
             axios
                 .post(`/api/tournament/${this.tournament.id}/notification`, {
                     before: this.inputs.before,
                     after: this.inputs.after,
                     interval: this.inputs.interval ? format(this.inputs.interval, 'HH:mm') : undefined,
+                    option: this.tournament.isRecurrent ? this.inputs.option : undefined,
+                    schedule
                 })
                 .then(() => {
                     this.tournamentStore.refresh();
@@ -92,40 +167,3 @@ export default {
     }
 }
 </script>
-
-<style>
-.v-select {
-    --vs-controls-color: #BFC9DB;
-    --vs-border-color: #333333;
-
-    --vs-dropdown-bg: #3E3E3E;
-    --vs-dropdown-color: #F2F5FA;
-    --vs-dropdown-option-color: #F2F5FA;
-
-    --vs-selected-bg: #333333;
-    --vs-selected-color: #F2F5FA;
-
-    --vs-search-input-color: #F2F5FA;
-
-    --vs-dropdown-option--active-bg: #333333;
-    --vs-dropdown-option--active-color: #F2F5FA;
-}
-
-.v-select .vs__dropdown-toggle {
-    height: 3rem;
-    border-radius: .5rem;
-    width: 100%;
-    border: 0;
-    margin-bottom: 0;
-
-    background-color: #4F4F4F;
-    color: #BFC9DB;
-    padding: 0 1rem;
-    box-sizing: border-box;
-}
-
-.v-select .vs__selected-options {
-    align-items: center;
-}
-
-</style>
