cc9a7b296b90eb80dc7b3ba2caaca8691e74362b
Mateus Rezende <mateusmendoncadr@gmail.com>
2022-10-03 23:17:20 -0300

    feat: versionamento da aplicação

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,7 +16,7 @@
     </script>
 
     <!-- Load Vue -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
     <!-- Load MercadoPago SDK -->
     <script src="https://sdk.mercadopago.com/js/v2"></script>
